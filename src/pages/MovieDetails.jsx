01a967960827de9d4deec2445bc4730ba1035168
sebaszwenger <sebaszwenger@gmail.com>
2022-04-28 10:12:17 -0300

    added the html of each MovieDetails page

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,3 +1,27 @@
+import movie from "./movie.json";
+import styles from "./MovieDetails.module.css";
+
 export default function MovieDetails() {
-  return <div>MovieDetails</div>;
+  const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+  return (
+    <div className={styles.MovieDetailsContainer}>
+      <img
+        className={`${styles.movieImage} ${styles.movieDetails}`}
+        src={imageUrl}
+        alt={movie.title}
+      />
+      <div className={styles.movieDetails}>
+        <p>
+          <strong>Title: </strong>
+          {movie.title}
+        </p>
+        <strong>Genres: </strong>
+        {movie.genres.map((genre) => genre.name).join(", ")}
+        <p>
+          <strong>Description: </strong>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
 }
